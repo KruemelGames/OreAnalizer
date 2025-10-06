7b2f99a4c17a7877573981cf3b3ee20dd5e2da53
KruemelGames <KruemelGames@gmail.com>
2025-10-06 10:18:25 +0200

    Delete easypeasyscanny directory

--- easypeasyscanny/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ['config', 'constants', 'scans', 'rocks', 'ui']
--- easypeasyscanny/rocks/models.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import Dict
-
-@dataclass
-class Rock:
-    id: str
-    composition: Dict[str, float]
-    mass: float
-    location: str
